cbcaba0e9516ee5627e2ab02b75eec2d2409ef8d
vini <vinicius.medeiros@dataside.com.br>
2023-02-14 08:51:33 -0300

    feat: finishing project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import { Xbox } from "./components/Xbox";
+
+export default function App() {
+  return (
+    <Flex w="100%" h="100vh" align="center" justify="center">
+      <Xbox />
+    </Flex>
+  );
+}
